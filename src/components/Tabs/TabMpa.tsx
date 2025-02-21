c17470e5f32caaed6147b3cb0a536b2b76aa0f55
Dmitry <melkov.dmitriy40@gmail.com>
2025-02-21 11:45:25 +0500

    fix

--- src/components/Tabs/TabMpa.tsx
+++ src/components/Tabs/TabMpa.tsx
@@ -1,5 +1,6 @@
 import BtnDefault from '../BtnDefault/BtnDefault';
 import { useCounter } from '../../hooks/CounterContext';
+import { useEffect, useState } from 'react';
 
 interface TabSushilkaProps {
   title: string;
@@ -8,9 +9,25 @@ interface TabSushilkaProps {
 const TabMpa: React.FC<TabSushilkaProps> = ({ title }) => {
   const { count, increment } = useCounter(); // Используем контекст
 
+  const [inputValue, setInputValue] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
+
+  useEffect(() => {
+    setDisplayValue(inputValue);
+  }, [inputValue]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <>
       <h1>{title}</h1>
+
+      <div>
+        <input type="text" value={inputValue} onChange={handleChange} />
+        <h2>Вы ввели: {displayValue}</h2>
+      </div>
 
       <div>
         <div>{count}</div>
